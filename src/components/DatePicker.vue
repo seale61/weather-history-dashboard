34218b461777d503b72b9488c5f46d701ba5e5ad
Charles Seale <c.seale@icloud.com>
2021-07-01 22:46:01 -0400

    New components for dates and state locations

---
+++ src/components/DatePicker.vue
@@ -0,0 +1,78 @@
+<template>
+    <v-menu
+        v-model="dateItem"
+        :close-on-content-click="false"
+        :nudge-right="40"
+        transition="scale-transition"
+        offset-y
+        min-width="auto"
+    >
+        <template v-slot:activator="{ on, attrs }">
+            <v-text-field
+                v-model="dateValue"
+                :label="dateLabel"
+                prepend-icon="mdi-calendar"
+                readonly
+                v-bind="attrs"
+                v-on="on"
+            ></v-text-field>
+        </template>
+        <v-date-picker
+            v-model="dateValue"
+            no-title
+            @input="setDate"
+        >
+        </v-date-picker>
+    </v-menu>
+</template>
+<script>
+
+import { mapState, mapActions } from 'vuex';
+export default {
+    name: 'DatePicker',
+    props: {
+        date: String,
+        dateLabel: String
+    },
+    computed: {
+        ...mapState([ 
+            'chartDates'
+        ]),
+    },
+    data() {
+        return {
+            dateItem:   false,
+            dateValue:  this.date
+        }
+    },
+    methods: {
+        ...mapActions([
+            'setChartDates'
+        ]),
+        setDate: function() {
+
+            this.dateItem = false;
+            let dates = null;
+
+            if (this.dateLabel === 'Start Date') {
+                //alert(this.dateLabel);
+                dates = {
+                    start: this.dateValue,
+                    end: this.chartDates.end
+                } 
+            } 
+            
+            if (this.dateLabel === 'End Date') {
+                //alert(this.dateLabel);
+                dates = {
+                    start: this.chartDates.start,
+                    end: this.dateValue
+                } 
+            }
+
+            this.setChartDates(dates);
+
+        }
+    }
+}
+</script>
---
+++ src/components/StationMenu.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="text-center">
+    <v-menu offset-y>
+      <template v-slot:activator="{ on, attrs }">
+        <v-btn
+          color="primary"
+          dark
+          v-bind="attrs"
+          v-on="on"
+        >
+          {{ currentStation }}
+        </v-btn>
+      </template>
+      <v-list>
+        <v-list-item
+          v-for="(station, index) in stations"
+          :key="index"
+          @click="loadCurrentStation(station)"
+        >
+          {{ station }}
+        </v-list-item>
+      </v-list>
+    </v-menu>
+  </div>
+</template>
+<script>
+    import { mapState, mapActions } from 'vuex';
+    export default {
+        name: 'StationMenu',
+        computed: {
+            ...mapState([ 
+                'stations',
+                'currentStation'
+            ]),
+        },
+        methods: {
+            ...mapActions([
+                'loadCurrentStation'
+            ])
+        }
+    }
+</script>
