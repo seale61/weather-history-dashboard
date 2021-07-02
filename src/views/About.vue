1106ec128c0dca37b94b66dc6aca5c65e607a6de
Charles Seale <c.seale@icloud.com>
2021-07-02 17:43:21 -0400

    Added content to About page

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,18 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
-  </div>
+    <div>
+        <v-card 
+          class="mx-auto mt-5" 
+          max-width="600" 
+          app 
+          color="secondary"
+        >
+          <v-card-title>Weather History Dashboard</v-card-title>
+          <v-card-subtitle>A working Demo</v-card-subtitle>
+          <v-card-text>
+            SealeWeb.Com<br>
+            seale61@gmail.com<br>
+            Copyright &copy; 2021, Charles Seale
+          </v-card-text>
+        </v-card>
+    </div>
 </template>
